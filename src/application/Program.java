185b0286f5395396c05c2ada0a147eb174f99968
brayansoaresalves <brayan.iub10@gmail.com>
2021-02-18 15:26:30 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		
 		Departament obj = new Departament(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Brayan", "brayan.iub10@gmail.com", new Date(), 1200.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
