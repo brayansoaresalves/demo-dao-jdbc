d20342fe91bd7433e0fc1acac712deda1cfb62df
brayansoaresalves <brayan.iub10@gmail.com>
2021-02-18 21:10:59 -0300

    Sellerdaojdbc e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(1, "Brayan", "brayan.iub10@gmail.com", new Date(), 1200.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
